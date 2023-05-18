eff08c638b1bddbba97a9b99d5240baa6d0bbbd0
Junya Kobayashi <jkobayashi.la2023@gmail.com>
2023-05-18 09:59:52 +0900

    新規作成（教材のショッピングサイト）

---
+++ src/main/java/com/example/demo/PersonalDev9999KobayshiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDev9999KobayshiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDev9999KobayshiApplication.class, args);
+	}
+
+}
